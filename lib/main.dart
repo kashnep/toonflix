e4792ab38f60cba5aa36c4777031a82c91ead2c9
JaeHyunEo <jaehyun0803@naver.com>
2023-10-25 00:36:31 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,38 +9,48 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.cyan.shade600,
-        body: Column(children: [
-          SizedBox(
-            //1. 80px 정도의 사각형 박스를 만든다.
-            height: 80,
-          ),
-          Row(
-            //2. 텍스트를
-            mainAxisAlignment: MainAxisAlignment.end,
+        backgroundColor: Color(0xFF181818),
+        body: Padding(
+          //4. 상하좌우 여백을 주기 위해 Column을 Padding 위젯 내 child로 설정한다.
+          padding: EdgeInsets.symmetric(
+              horizontal:
+                  40), // all : 사방을 지정 / only : 특정 방향 지정 right, left... / symmetric : 수평/수직으로 나누어 지정
+          child: Column(
             children: [
-              Column(
+              SizedBox(
+                //1. 80px 정도의 사각형 박스를 만든다.
+                height: 80,
+              ),
+              Row(
+                //2. Row를 우측으로 보낸다.
+                mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  Text(
-                    'Hey, Selena',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 38,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  Text(
-                    'Welcome Back!',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 48,
-                    ),
+                  Column(
+                    //3. Column 위젯 내 텍스트 위젯을 우측으로 보낸다.
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Hey, Selena',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 38,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Text(
+                        'Welcome Back!',
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(0.8), // 투명도 설정
+                          fontSize: 22,
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
             ],
           ),
-        ]),
+        ),
       ),
     );
   }
